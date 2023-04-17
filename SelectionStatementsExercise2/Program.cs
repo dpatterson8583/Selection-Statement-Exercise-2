3e50d21c2515847b56425fcbe66d76abc719df6a
dpatterson8583 <dpatterson8583@gmail.com>
2023-04-17 11:44:39 -0500

    Initial commit for Selection stmt #2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string subject;
+            Console.Write("What is your favorite subject in school? >");
+            subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "History": 
+                    {
+                        Console.WriteLine($"{subject}...Nothing new here.");
+                        break;
+                    }
+                case "Math":
+                    {
+                        Console.WriteLine($"{subject}...It was hard!");
+                        break;
+                    }
+                case "Geography":
+                    {
+                        Console.WriteLine($"{subject}...Cool places to go.");
+                        break;
+                    }
+                case "English":
+                    {
+                        Console.WriteLine($"{subject}...Learned to write and speak correctly.");
+                        break;
+                    }
+                case "PE":
+                    {
+                        Console.WriteLine($"{subject}...I could live without it.");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine($"{subject} was something else...");
+                        break;
+                    }
+            }
+
+
+
+            
         }
     }
 }
